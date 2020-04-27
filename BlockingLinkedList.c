8211954fe91cca7ef90d7ea674c0f62b92271bc6
Ian Chou <ianchou19@gmail.com>
2020-04-26 20:28:01 -0400

    Blocking Linked List 2

--- BlockingLinkedList.c
+++ BlockingLinkedList.c
@@ -7,8 +7,9 @@
 #include <time.h>
 
 volatile unsigned long counter = 0;
-
 static pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+static pthread_mutex_t mutex3 = PTHREAD_MUTEX_INITIALIZER;
 
 int randomNumber(){
     return((1 + (int)( 20.0 * rand() / ( RAND_MAX + 1.0 ) )));
@@ -20,71 +21,65 @@ typedef struct Elem {
         struct Elem *next;
 }elem;
 
+static elem *start;
+
+void create(elem **head) {
+    (*head) = malloc(sizeof(elem)); // dummy head
+    (*head)->next = NULL;
+}
+
 void insert(elem *pointer, int data) {
     /* Iterate through the list till we encounter the last elem.*/
-    while(pointer->next != NULL && (pointer->next)->data < data) {
+    while(pointer->next != NULL) {
         pointer = pointer -> next;
     }
     /* Allocate memory for the new elem and put data in it.*/
-    // pointer->next = (elem *)malloc(sizeof(elem));
-    // pointer = pointer->next;
-    // pointer->data = data;
-    // pointer->next = NULL;
+    pointer->next = (elem *)malloc(sizeof(elem));
+    pointer->next->data = data;
+    pointer->next->next = NULL;
 }
 
-
-int finding(elem *pointer, int key) {
-    pointer = pointer -> next;
-    /* Iterate through the entire linked list and search for the key. */
-    pointer = pointer -> next;
-
-    while(pointer != NULL) {
-        if(pointer->data == key) {
-            return 1;
-        }
-        pointer = pointer -> next;
+int finding(elem *pointer, int data) {
+    while (pointer->next != NULL && (pointer->next)->data != data) {
+            pointer = pointer -> next;
     }
-    /*Key is not found */
-    return 0;
+
+    if (pointer->next != NULL) {
+        // printf("Element %d is found in the list\n",data);
+        return true;
+    } else {
+        return false;
+    }
 }
 
 void delete(elem *pointer, int data) {
-    printf("Delete da data = %d\n", data);
     /* Go to the elem for which the elem next to it has to be deleted */
     while(pointer->next != NULL && (pointer->next)->data != data) {
             pointer = pointer -> next;
     }
+
     if(pointer->next==NULL) {
-            printf("Element %d is not present in the list\n",data);
+            // printf("Element %d is not present in the list\n",data);
             return;
     }
-    /* Now pointer points to a elem and the elem next to it has to be removed */
+
     elem *temp;
     temp = pointer -> next;
-    /*temp points to the elem which has to be removed*/
     pointer->next = temp->next;
-    /*We removed the elem which is next to the pointer (which is also temp) */
     free(temp);
-    /* Beacuse we deleted the elem, we no longer require the memory used for it . 
-        free() will deallocate the memory.
-        */
+
     return;
 }
 
 void print(elem *pointer) {
     pointer = pointer->next;
-    printf("Print Sequential\n");
+    // printf("Print Sequential\n");
     while(pointer != NULL){
         printf("%d ",pointer->data);
         pointer = pointer->next;
     }
-    printf("\n\n");
+    printf("\n");
     return;
-}
-
-void create(elem **head) {
-    (*head) = malloc(sizeof(elem));
-    (*head)->next = NULL;
 }
 
 bool nextBool() {
@@ -97,8 +92,6 @@ bool nextBool() {
         return false;
 }
 
-static elem *start;
-
 void *worker(void *arg) {
     int i = 0;
     int aux = 0;
@@ -107,20 +100,26 @@ void *worker(void *arg) {
         /*If true generates insert, otherwise generates remove*/
         if(nextBool()){
             aux = randomNumber();
+
+            pthread_mutex_lock(&mutex1);
             insert(start, aux);
+            pthread_mutex_unlock(&mutex1);
 
             printf("Insert = %d\n",aux);
             printf("The ID of this thread is: %u\n", (unsigned int)pthread_self());
         } else {
-            // aux = randomNumber();
-            // delete(start, aux);
+            aux = randomNumber();
 
-            // printf("Deleted = %d\n",aux);
-            // printf("The ID of this thread is: %u\n", (unsigned int)pthread_self());
+            pthread_mutex_unlock(&mutex2);
+            delete(start, aux);
+            pthread_mutex_unlock(&mutex2);
+
+            printf("Deleted = %d\n",aux);
+            printf("The ID of this thread is: %u\n", (unsigned int)pthread_self());
         }
-        pthread_mutex_lock(&mutex1);
+        pthread_mutex_lock(&mutex3);
         print(start);
-        pthread_mutex_unlock(&mutex1);
+        pthread_mutex_unlock(&mutex3);
     }
     return NULL;
 }
@@ -149,11 +148,7 @@ int main(int argc, char **argv) {
     for (i = 0; i < atoi(argv[1]); ++i)
         assert(!pthread_join(t[i], NULL));
 
-    /* print counter value */
-    printf("all thread done -> counter=%lu\n", counter);
-
-    print(start);
-
+    printf("all thread done\n");
 
     return 0;
 }
